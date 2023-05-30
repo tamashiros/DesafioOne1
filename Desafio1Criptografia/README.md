84b88fe2d9cf6f592993defca9d5da7f7b79d78a
Keiichi Tamashiro <127620213+tamashiros@users.noreply.github.com>
2023-05-29 21:03:38 -0300

    Importei arquivo
    Desafio terminado

---
+++ Desafio1Criptografia/README.md
@@ -0,0 +1,25 @@
+# Challenge ONE - Iniciante em Programação
+Desencriptador foi um projeto em que pude colocar em prática e consolidar os conhecimentos adquiridos através dos cursos de JavaScript, HTML e CSS3.
+
+![print](https://github.com/ThaisRivarolli/projeto-encriptador/assets/94804477/3767541a-9236-4d68-a541-d19144057588)
+
+
+
+# Requisitos do desafio:
+
+✓ Criptografar mensagens utilizando um algoritmo sugerido nos requisitos do projeto.
+
+✓ Descrever mensagens previamente criptografadas.
+
+✓ Copiar mensagens criptografadas ou descriptografadas para a área de download.
+ 
+
+ 
+ # Tecnologias utilizadas:
+ 
+![image](https://github.com/ThaisRivarolli/projeto-encriptador/assets/94804477/53ec0b00-98ba-4d6c-b4b6-511a0613cb33)
+![image](https://github.com/ThaisRivarolli/projeto-encriptador/assets/94804477/f7e8ed9b-4153-46c6-be83-7c206ab9d718)
+![image](https://github.com/ThaisRivarolli/projeto-encriptador/assets/94804477/a906d5d0-f0a7-4376-87cf-7f78add6f06a)
+
+
+![one](https://github.com/ThaisRivarolli/projeto-encriptador/assets/94804477/4c312a5a-cf6e-4b19-8d47-3a53c9a7e77d)
---
+++ Desafio1Criptografia/script.js
@@ -0,0 +1,54 @@
+const textArea = document.querySelector(".text-area");
+const mensagem = document.querySelector(".mensagem");
+const btnCopiar = document.querySelector(".mensagem");
+
+
+function btnEncriptar () {
+    const textoEncriptado = encriptar(textArea.value);
+    mensagem.value = textoEncriptado;
+    textArea.value = "";
+}
+
+function encriptar (stringEncriptada) {
+    let matrizCodigo = [["e", "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0] , matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+function btnDesencriptar () {
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensagem.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar (stringDesencriptada) {
+    let matrizCodigo = [["e", "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1] , matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+
+
+let btn = document.querySelector('#btn-copiar')
+
+btn.addEventListener('click', function(e){
+    let textArea = document.querySelector('.mensagem')
+    navigator.clipboard.writeText(textArea.value);
+    btn.textContent = "Copiar"
+    
+    alert("Texto copiado com sucesso!");
+});
